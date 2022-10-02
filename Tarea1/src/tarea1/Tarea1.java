3c0934f1983abfcd6b54d8c0ecd8e76e3afd5041
Ricachi <nicolasricciardi2002@gmail.com>
2022-10-02 00:50:04 -0100

    Prueba hola mundo en 'git'

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tarea1;
 
-/**
- *
- * @author Nicolas
- */
 public class Tarea1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Prueba");
     }
     
 }
