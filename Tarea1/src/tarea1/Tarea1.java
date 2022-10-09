eee5fd35e6dda89cde995a39aed0bb8134cea2ee
MattelXD <matiashernanxd@gmail.com>
2022-10-09 18:21:12 -0300

    Mas cosas en el main

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -21,7 +21,7 @@ public class Tarea1 {
          v1.calcPeso();
          v1.setCantidad(1);
          v1.getCantidad();
-         System.out.println("Usted ha pedido un/una " +ar1.getNombre()+ " \nSu peso es: " +v1.calcPeso()+ "\nSu precio es: " +v1.calcPrecioSinIVA()+ "\nCon " +v1.calcIVA()+ "% de IVA" +"\nEl total es : " +v1.calcPrecio());
+         System.out.println("Usted ha pedido un/unas " +ar1.getNombre()+ " \nSu peso es: " +v1.calcPeso()+ "\nSu precio es: " +v1.calcPrecioSinIVA()+ "\nCon " +v1.calcIVA()+ "% de IVA" +"\nEl total es : " +v1.calcPrecio());
          
          OrdenCompra ord1 = new OrdenCompra(v1);
          ord1.calcIVA();
@@ -35,6 +35,37 @@ public class Tarea1 {
          ord1.getEstado();
          String s = ord1.toString();
          System.out.println(s);
+         
+         System.out.print("\n");
+         
+         Articulo ar2 = new Articulo();
+         ar2.setPeso(1);
+         ar2.setPrecio(5);
+         ar2.setNombre("Galletas");
+         ar2.setDescripcion("Galletas sabor a Coco");
+         System.out.println(ar2.toString());
+         DetalleOrden v2 = new DetalleOrden(ar2);
+         
+          v2.calcPrecio();
+         v2.calcPrecioSinIVA();
+         v2.calcIVA();
+         v2.calcPeso();
+         v2.setCantidad(5);
+         v2.getCantidad();
+         System.out.println("Usted ha pedido un/uns " +ar2.getNombre()+ " \nSu peso es: " +v2.calcPeso()+ "\nSu precio es: " +v2.calcPrecioSinIVA()+ "\nCon " +v2.calcIVA()+ "% de IVA" +"\nEl total es : " +v2.calcPrecio());
+         
+         OrdenCompra ord2 = new OrdenCompra(v2);
+         ord2.calcIVA();
+         ord2.calcPrecio();
+         ord2.calcPrecioSinIVA();
+         ord2.Peso();
+         Date fecha2 = new Date(12, 05, 2022);
+         ord2.setFecha(fecha2);
+         ord2.getFecha();
+         ord2.setEstado("La compra ha sido procesada exitosamente");
+         ord2.getEstado();
+         String s1 = ord2.toString();
+         System.out.println(s1);
         
 
      }
